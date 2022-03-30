3d5971b20a508c70fb8f3566cda23f0253abae22
Ruby <LIU10032@163.com>
2022-03-30 22:55:56 +0800

    feat: layout of the login view

---
+++ Bankkkey/Bankkkey/Login/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Bankkkey
+//
+//  Created by Ruby on 2022/3/30.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+        layout()
+    }
+
+
+}
+
+
+extension LoginViewController {
+    private func style(){
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout(){
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
