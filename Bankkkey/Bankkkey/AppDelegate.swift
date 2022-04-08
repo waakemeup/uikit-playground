bf0e4cb437669caedeac5a05411a74b3593aff7a
Ruby <LIU10032@163.com>
2022-04-08 14:57:37 +0800

    feat: add tranisition

--- Bankkkey/Bankkkey/AppDelegate.swift
+++ Bankkkey/Bankkkey/AppDelegate.swift
@@ -34,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate:LoginViewControllerDelegate {
     func didLogin() {
-        print("foo - Did login")
+        setRootViewController(onboardingContainerViewController)
     }
 }
 
@@ -42,5 +42,19 @@ extension AppDelegate:LoginViewControllerDelegate {
 extension AppDelegate:OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
         print("foo - Did onboard")
+    }
+}
+
+extension AppDelegate {
+    func setRootViewController(_ vc:UIViewController,animated:Bool = true){
+        guard animated,let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+        
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window, duration: 0.7, options: .transitionCrossDissolve,animations: nil,completion: nil)
     }
 }
