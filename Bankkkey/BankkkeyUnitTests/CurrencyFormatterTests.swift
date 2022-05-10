cc1bfd5742d172593475d918bff949c3b2afb87e
Ruby <LIU10032@163.com>
2022-05-10 23:25:55 +0800

    add: Swift Unit Test Module

---
+++ Bankkkey/BankkkeyUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyFormatterTests.swift
+//  BankkkeyUnitTests
+//
+//  Created by Ruby on 2022/5/10.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankkkey
+
+class Test:XCTestCase {
+    var formatter:CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+    
+    func testShouldBeVisible() throws{
+        
+    }
+}
