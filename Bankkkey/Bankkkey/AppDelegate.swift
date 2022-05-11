37e0991892d4fe31eb0f62551c0fc8a5d49b3996
Ruby <LIU10032@163.com>
2022-05-11 23:23:16 +0800

    feat: setup viewcontroller

--- Bankkkey/Bankkkey/AppDelegate.swift
+++ Bankkkey/Bankkkey/AppDelegate.swift
@@ -29,15 +29,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
 
-        let vc = mainViewController
-        vc.setStatusBar()
+//        let vc = mainViewController
+//        vc.setStatusBar()
+//
+//        UINavigationBar.appearance().isTranslucent = false
+//        UINavigationBar.appearance().backgroundColor = appColor
+//
+//        window?.rootViewController = vc
+//
+//        return true
         
+        displayLogin()
+        return true
+    }
+    
+    private func displayLogin(){
+        setRootViewController(loginViewController)
+    }
+    
+    private func displayNextScreen(){
+        if LocalState.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainViewController)
+        } else {
+            setRootViewController(onboardingContainerViewController)
+        }
+    }
+    
+    private func prepMainView(){
+        mainViewController.setStatusBar()
         UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().backgroundColor = appColor
-        
-        window?.rootViewController = vc
-        
-        return true
     }
 }
 
@@ -58,17 +80,14 @@ extension AppDelegate {
 
 extension AppDelegate:LoginViewControllerDelegate {
     func didLogin() {
-        if LocalState.hasOnboarded{
-            setRootViewController(mainViewController)
-        }else {
-            setRootViewController(onboardingContainerViewController)
-        }
+        displayNextScreen()
     }
 }
 
 extension AppDelegate:OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
         LocalState.hasOnboarded = true
+        prepMainView()
         setRootViewController(mainViewController)
     }
 }
