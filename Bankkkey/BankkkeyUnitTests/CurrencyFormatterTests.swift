befa2a12d26af7fe302f2aeb100eab1dec465660
Ruby <LIU10032@163.com>
2022-05-11 22:16:11 +0800

    test: currency formatting

--- Bankkkey/BankkkeyUnitTests/CurrencyFormatterTests.swift
+++ Bankkkey/BankkkeyUnitTests/CurrencyFormatterTests.swift
@@ -18,7 +18,27 @@ class Test:XCTestCase {
         formatter = CurrencyFormatter()
     }
     
-    func testShouldBeVisible() throws{
+    func testBreakDollorsIntoCents() throws{
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0,"929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "$929,466.23")
+    }
+    
+    func testZeroDollarsFormatted() throws{
+        let result = formatter.dollarsFormatted(00.0)
+        XCTAssertEqual(result, "$0.00")
+    }
+    
+    func testDollarsFormattedWithCurrencySymbol() throws{
+        let locale = Locale.current
+        let currencySymbol = locale.currencySymbol!
         
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "\(currencySymbol)929,466.23")
     }
 }
