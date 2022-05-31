799e1c7d6929094856f669a9712f89cdea0cbe97
Ruby <LIU10032@163.com>
2022-05-31 21:09:05 +0800

    feat: draw the basic table

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -14,14 +14,42 @@ class ChecklistViewController: UITableViewController {
     // Do any additional setup after loading the view.
   }
   
-  
+  // MARK: - Table View Data Source
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return 100
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+    
+    let label = cell.viewWithTag(1000) as! UILabel
+    
+    if indexPath.row % 5 == 0 {
+      label.text = "Walk the dog"
+    } else if indexPath.row % 5 == 1 {
+      label.text = "have fun"
+    } else if indexPath.row % 5 == 2 {
+      label.text = "Learn Swift"
+    } else if indexPath.row % 5 == 3 {
+      label.text = "Watch WWDC"
+    } else if indexPath.row % 5 == 4 {
+      label.text = "Eat ice cream"
+    }
+    
     return cell
+  }
+  
+  //MARK: - Table View Delegate
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if let cell = tableView.cellForRow(at: indexPath) {
+      if cell.accessoryType == .none {
+        cell.accessoryType = .checkmark
+      } else {
+        cell.accessoryType = .none
+      }
+    }
+    
+    tableView.deselectRow(at: indexPath, animated: true)
   }
 }
 
